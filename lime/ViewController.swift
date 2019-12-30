94e6d5c3d11ee1dc713eabd0f61801d24fba381f
D1438 <b1018172@fun.ac.jp>
2019-12-31 02:25:12 +0900

    Initialize

---
+++ lime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lime
+//
+//  Created by Ishizawa Daisuke on 2019/12/31.
+//  Copyright © 2019 Ishizawa Daisuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
